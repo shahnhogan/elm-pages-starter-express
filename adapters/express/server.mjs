c43db7503823ceb647d8c84e44ead94496f06959
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-08-27 09:05:47 +0200

    Add logging using morgan

--- adapters/express/server.mjs
+++ adapters/express/server.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 import elmPagesMiddleware from "./middleware.mjs";
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.static("dist"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(elmPagesMiddleware);
+app.use(morgan("combined"));
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
